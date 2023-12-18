3c85e9e0af6b38190ef2f30fa85e0b83e3ab60d4
Hari-Rait <86234153+Hari-Rait@users.noreply.github.com>
2023-12-18 22:50:21 +0100

    Calendar View UI

---
+++ NeedsToPay/Helpers/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  NeedsToPay
+//
+//  Created by Hari Rait on 18.12.23.
+//
+
+import SwiftUI
+
+struct Constants: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Constants()
+}
