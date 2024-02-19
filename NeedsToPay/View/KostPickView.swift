f251f628b5e49ad3728699168436eed3cdd944bc
Hari-Rait <86234153+Hari-Rait@users.noreply.github.com>
2024-02-19 21:44:54 +0100

    New View in Peoples View

---
+++ NeedsToPay/View/KostPickView.swift
@@ -0,0 +1,18 @@
+//
+//  KostPickView.swift
+//  NeedsToPay
+//
+//  Created by Hari Rait on 15.02.24.
+//
+
+import SwiftUI
+
+struct KostPickView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KostPickView()
+}
